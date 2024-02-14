aeb0c006c98286e758fa2c856da763854bf99b6d
footprintbeaver <tkdlzhmk3@gmail.com>
2024-02-15 03:01:23 +0900

    [Feat]에셋추가& 보상UI

---
+++ Assets/ScriptableObjects/Scripts/Reward/ItemData.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemType
+{
+    Melee,
+    Range,
+    Consumable,
+    Accesory
+}
+
+public enum ConsumableType
+{
+    Health,
+    Bomb,
+    Magic
+}
+
+[System.Serializable]
+public class ItemDataConsumable
+{
+    public ConsumableType type;
+    public float value;
+}
+
+[CreateAssetMenu(fileName = "Item", menuName = "New Item")]
+public class ItemData : ScriptableObject
+{
+    [Header("Info")]
+
+    public string displayName;
+    public string description;
+    public ItemType type;
+    public Sprite icon;
+
+    [Header("Stacking")]
+    public bool canStack;
+    public int maxStackAmount;
+
+    [Header("Consumable")]
+    public ItemDataConsumable[] consumables;
+
+    [Header("Weapon")]
+    public GameObject projectile;
+
+}
